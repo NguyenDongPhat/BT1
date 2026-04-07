87b31cce4d7c06c8286f23308a608f95d27f40c8
NguyenDongPhat <phatnd04@gmail.com>
2026-04-07 14:01:47 +0700

    commit 4

--- interactivitytoapp/bin/interactivitytoapp.dart
+++ interactivitytoapp/bin/interactivitytoapp.dart
@@ -1,5 +1,35 @@
-import 'package:interactivitytoapp/interactivitytoapp.dart' as interactivitytoapp;
+
+import 'dart:io';
+
+const version = '0.0.1';
 
 void main(List<String> arguments) {
-  print('Hello world: ${interactivitytoapp.calculate()}!');
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
+  }
+}
+
+void printUsage() {
+  print("Commands: help, version, search <ten>");
+}
+
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  if (arguments == null || arguments.isEmpty) {
+    print('Nhap noi dung can tim:');
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Dang tim: "$articleTitle"...');
+  print('Tim thay!');
 }
